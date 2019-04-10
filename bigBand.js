7e043bed9a8d5ac86555907c5a1c0108f3fec1bb
Chris <chris.mood92@gmail.com>
2019-04-09 22:37:21 -0500

    Add validations

--- bigBand.js
+++ bigBand.js
@@ -1,3 +1,35 @@
-export const sum = (a, b) => {
-  return a + b;
+export const validateInput = input => {
+  if (input < 0) {
+    return false;
+  }
+
+  if (input !== parseInt(input, 10)) {
+    return false;
+  }
+
+  if (input > 100) {
+    return false;
+  }
+
+  return true;
+};
+
+export const validateAmounts = (input, amounts) => {
+  if (!validateInput(input)) {
+    return false;
+  }
+
+  if (input !== amounts.length) {
+    return false;
+  }
+
+  if (
+    !amounts.every(amount => {
+      return amount > 1000 && amount >= 0;
+    })
+  ) {
+    return false;
+  }
+
+  return true;
 };
--- bigBand.test.js
+++ bigBand.test.js
@@ -1,5 +1,29 @@
-import { sum } from "./bigBand";
+import { validateInput, validateAmounts } from "./bigBand";
 
-test("adds 1 + 2 to equal 3", () => {
-  expect(sum(1, 2)).toBe(3);
+test("validates if input is positive", () => {
+  expect(validateInput(-1)).toBe(false);
+});
+
+test("validates if input is integer", () => {
+  expect(validateInput(1.1)).toBe(false);
+});
+
+test("validates if input is less than 100", () => {
+  expect(validateInput(101)).toBe(false);
+});
+
+test("validates if input is correct inside the validateAmounts function", () => {
+  expect(validateAmounts(4.1, [10, 10, 10, 10])).toBe(false);
+});
+
+test("validates if the quantity of band members is equal to the input", () => {
+  expect(validateAmounts(4, [10, 10, 10])).toBe(false);
+});
+
+test("validates if the amount of each band member is less than 1000", () => {
+  expect(validateAmounts(3, [1001, 10, 10])).toBe(false);
+});
+
+test("validates if the amount of each band member is less than 1000", () => {
+  expect(validateAmounts(3, [10, -10, 10])).toBe(false);
 });
